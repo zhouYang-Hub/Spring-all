c48c0870b44e73196ec5cef5595af720ec60c3f3
zhouyang <zhouyang@>
2024-03-07 11:22:30 +0800

    git 第一次提交

--- xiancheng/src/main/java/com/spring/zhouyang/xiancheng/git/Git.java
+++ xiancheng/src/main/java/com/spring/zhouyang/xiancheng/git/Git.java
@@ -10,4 +10,7 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("git 第一次提交");
     }
+    public static void commit2(String[] args) {
+        System.out.println("git 第二次提交");
+    }
 }
